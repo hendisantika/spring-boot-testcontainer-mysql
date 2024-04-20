a9412a627ac1595dafefa82891b97850ba56497b
hendisantika <hendisantika@yahoo.co.id>
2024-04-20 08:22:31 +0700

    #13 - Bug Fix Integration Test :pencil2: :green_book: :four:🫸🌀✏️📗🐧🐳

--- src/test/java/com/hendisantika/SpringBootTestcontainerMysqlApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootTestcontainerMysqlApplicationTests.java
@@ -1,13 +1,39 @@
 package com.hendisantika;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:8:///test"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootTestcontainerMysqlApplicationTests {
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @Test
-    void contextLoads() {
+    void createNewBook() {
+        Book book = new Book();
+        Author author = new Author();
+        author.setName("Itadori Yuji");
+
+        book.setAuthor(author);
+        book.setTitle("Mudah belajar Sihir");
+
+        bookRepository.save(book);
+
+        long count = bookRepository.count();
+        Assertions.assertEquals(1, count);
     }
 
 }
